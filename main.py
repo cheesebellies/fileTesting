238e568942c3519ca8cfb1d4bcf34a519466bad0
Replit user <>
2022-09-30 12:50:27 +0000

    more small changes to main.py, comment out selenium for testing purposes

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import requests
-import selenium
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import json
-import time
+# import selenium
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# import json
+# import time
 
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_experimental_option("detach", True)
+# chrome_options = Options()
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--disable-dev-shm-usage')
+# chrome_options.add_experimental_option("detach", True)
   
-driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome(options=chrome_options)
 
 
 url = (requests.post("https://seltest2.david0weir.repl.co/search",{'tosearch':'moana'}).text)
 
-
+print(url)
 # driver.get(url)
