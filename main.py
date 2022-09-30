2c13c2622a7604f1da6ff9357ba63f787b4d0308
Replit user <>
2022-09-30 15:54:59 +0000

    change main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.common.keys import Keys
 # import json
-# import time
+import time
 
 # chrome_options = Options()
 # chrome_options.add_argument('--no-sandbox')
@@ -13,8 +13,11 @@ import requests
 # chrome_options.add_experimental_option("detach", True)
   
 # driver = webdriver.Chrome(options=chrome_options)
+l = ["seltest2","middleman","lastman"]
+for i in l:
+  requests.get(f"https://{i}.david0weir.repl.co/")
 
-
+time.sleep(20)
 url = (requests.post("https://seltest2.david0weir.repl.co/search",{'tosearch':'moana'}).text)
 
 print(url)
