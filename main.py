3b7786bda5494140811fb4e1c90433c5e2fd6e3e
Replit user <>
2022-10-01 19:51:39 +0000

    changes to main.py

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import requests
-# import selenium
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# import json
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import json
 import time
-
-# chrome_options = Options()
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--disable-dev-shm-usage')
-# chrome_options.add_experimental_option("detach", True)
   
-# driver = webdriver.Chrome(options=chrome_options)
-l = ["seltest2","middleman","lastman"]
-for i in l:
-  requests.get(f"https://{i}.david0weir.repl.co/")
+# l = ["seltest2","middleman","lastman"]
+# for i in l:
+#   requests.get(f"https://{i}.david0weir.repl.co/")
 
-time.sleep(20)
-url = (requests.post("https://seltest2.david0weir.repl.co/search",{'tosearch':'moana'}).text)
+# url = (requests.post("https://seltest2.david0weir.repl.co/search",{'tosearch':'moana'}).text)
 
-print(url)
-# driver.get(url)
+# print(url)
+
+url = "https://streamlare.com/e/10rwmD79oYdlRaZ8"
+
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+# chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get(url)
+
+print(driver.get_cookies())
