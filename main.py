215281a7a6c2fea22ceadc10d0ab0329d6c3b770
Replit user <>
2022-10-01 20:03:36 +0000

    change main.py

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import json
 import time
+import ast
   
-# l = ["seltest2","middleman","lastman"]
-# for i in l:
-#   requests.get(f"https://{i}.david0weir.repl.co/")
+l = ["seltest2","middleman","lastman"]
+for i in l:
+  requests.get(f"https://{i}.david0weir.repl.co/")
 
-# url = (requests.post("https://seltest2.david0weir.repl.co/search",{'tosearch':'moana'}).text)
+url = (requests.post("https://seltest2.david0weir.repl.co/search",{'tosearch':'moana'}).text)
+cookies = requests.get("https://lastman.david0weir.repl.co/cookies")
+cookies = ast.literal_eval(cookies)
 
-# print(url)
-
-url = "https://streamlare.com/e/10rwmD79oYdlRaZ8"
+print(url)
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
@@ -24,6 +25,9 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get(url)
+driver.get("https://www.google.com/")
 
-print(driver.get_cookies())
+for i in cookies:
+  driver.add_cookie(i)
+
+driver.get(url)
