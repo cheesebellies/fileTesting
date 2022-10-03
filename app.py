5039042a5f6124ac230909971a09025f6da5b121
Replit user <>
2022-10-03 23:54:12 +0000

    oh mugod its a workin

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import os
 from flask import Flask, send_from_directory
+from multiprocessing import Process
+import requests
+import time
+import os
+
+def busytask():
+  while True:
+    time.sleep(20)
+    requests.get("https://google.com")
+    os.system("./busywork.sh")
+
+p = Process(target=busytask)
+p.daemon = True
+p.start()
 
 app = Flask(__name__, static_url_path='')
 
--- run.py
+++ run.py
@@ -18,54 +18,80 @@ from flask import Flask, send_from_directory
 from replit import db
 
 db["isdone"]=False
+db["progress"] = 1
+db["info"] = ''
 
-l = ["seltest2","middleman","lastman"]
-for i in l:
-  requests.get(f"https://{i}.david0weir.repl.co/")
+class Downloader:
 
-url = (requests.post("https://seltest2.david0weir.repl.co/search",{'tosearch':sys.argv[1]}).text)
+  def __init__(self):
+    pass
+    
+  def initp(self):
+    db["progress"] = 2
+    l = ["seltest2","middleman","lastman"]
+    for i in l:
+      requests.get(f"https://{i}.david0weir.repl.co/")
 
-# cookies = requests.get("https://lastman.david0weir.repl.co/cookies").text
-# print(cookies)
-# cookies = ast.literal_eval(cookies)
-# print(cookies)
+  def geturl(self,info):
+    db["progress"] = 3
+    db["info"] = info
+    url = (requests.post("https://seltest2.david0weir.repl.co/search",{'tosearch':info}).text)
+    return url
+  
+  # cookies = requests.get("https://lastman.david0weir.repl.co/cookies").text
+  # print(cookies)
+  # cookies = ast.literal_eval(cookies)
+  # print(cookies)
+  def dlurl(self,url):
+    print(url)
+    db["progress"] = 4
+    db["info"] = str(url)
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_experimental_option("detach", True)
+    
+    driver = webdriver.Chrome(options=chrome_options)
+    
+    # driver.get("https://www.google.com/")
+    
+    # for i in cookies:
+    #   driver.add_cookie(i)
+    
+    
+    driver.get(url)
+    
+    result = driver.find_element(By.TAG_NAME, "video").get_attribute("src")
+    print(result)
+    driver.get(result)
+    sa = 0
+    time.sleep(10)
+    bar = Bar()
+    p = Process(target=bar.Start)
+    p.daemon = True
+    p.start()
+    while True:
+      sa+= 1
+      time.sleep(300)
+      driver.get("https://google.com")
+      if sa == 10:
+        break
+    
+    while True:
+      time.sleep(5)
+      if db["isdone"] == True:
+        print("GO TO https://FILETEST.david0weir.repl.co/vod to download video!!")
+        break
 
-print(url)
 
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_experimental_option("detach", True)
+class Start:
 
-driver = webdriver.Chrome(options=chrome_options)
+  def __init__(self,tosearch):
+    self.dlr = Downloader()
+    self.tosearch = tosearch
+    
 
-driver.get("https://www.google.com/")
-
-# for i in cookies:
-#   driver.add_cookie(i)
-
-
-driver.get(url)
-
-result = driver.find_element(By.TAG_NAME, "video").get_attribute("src")
-print(result)
-
-driver.get(result)
-sa = 0
-time.sleep(10)
-bar = Bar()
-p = Process(target=bar.Start)
-p.daemon = True
-p.start()
-while True:
-  sa+= 1
-  time.sleep(300)
-  driver.get("https://google.com")
-  if sa == 8:
-    break
-
-while True:
-  time.sleep(5)
-  if db["isdone"] == True:
-    print("GO TO https://FILETEST.david0weir.repl.co/vod to download video!!")
-    break
+  def start(self):
+    self.dlr.initp()
+    url = self.dlr.geturl(self.tosearch)
+    self.dlr.dlurl(url)
